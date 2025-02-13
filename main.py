02f7b034a848a3c235d2d8174c2b75995712a5ad
Chief_Ace <eraytopal61@hotmail.com>
2025-02-13 23:09:55 +0800

    Added Ball Object, and fixed documentation file name

--- main.py
+++ main.py
@@ -15,9 +15,47 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
 
+
+# Ball Class
+class Ball:
+    # Initialising Ball object
+    def __init__(self, x, y, radius, speed_x, speed_y):
+        self.x = x # Ball x-coord
+        self.y = y # Ball y-coord
+        self.radius = radius # Ball radius
+        self.speed_x = speed_x # Ball speed x-component
+        self.speed_y = speed_y # Ball speed y-component
+
+    # Movement of ball
+    def move(self):
+        self.x += self.speed_x
+        self.y += self.speed_y
+
+        # Collision with walls => Collide with walls, then go opposite direction
+        if self.x <= self.radius or self.x >= SCREEN_WIDTH - self.radius:
+            self.speed_x *= -1
+        if self.y <= self.radius:
+            self.speed_y *= -1
+    
+
+    # Function with drawing ball on actual screen
+    def draw(self):
+        pygame.draw.circle(screen, RED, (int(self.x), int(self.y)), self.radius)
+
+    # Resetting the ball
+    def reset(self):
+        self.x = SCREEN_WIDTH // 2
+        self.y = SCREEN_HEIGHT // 2
+        self.speed_x = 5 * random.choice([-1, 1])
+        self.speed_y = -5
+
+
+
+
 # Game loop
 running = True
 while running:
+
     # Event handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
